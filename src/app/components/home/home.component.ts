d80939f89b29d0040962cdb526b0ce9ac546bef4
Pamela Erraes <pamela_erraez@hotmail.com>
2025-03-21 15:20:02 -0500

    He creado para obtener todos  los usuarios

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IUser } from '../../interfaces/iuser.interfaces';
+import { UserService } from '../../services/user.service';
+import Swal from 'sweetalert2'
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  arrUsuarios: IUser[] = [];
+  apiService = inject(UserService);
 
+  // Cargar usuarios automáticamente al inicializar el componente
+ ngOnInit(){
+
+  this.apiService.getAll().subscribe( response =>{
+    this.arrUsuarios = response.results;
+    //console.log(response.results)
+
+  })
+}
+
+deleteUser(id: number) {
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: 'Esta acción no se puede deshacer',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Sí, eliminar',
+    cancelButtonText: 'Cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.apiService.deleteUser(id).subscribe(() => {
+        this.arrUsuarios = this.arrUsuarios.filter(user => user.id !== id);
+
+        // Mostrar mensaje de éxito
+        Swal.fire(
+          'Eliminado',
+          'El usuario ha sido eliminado correctamente',
+          'success'
+        );
+      });
+    }
+  });
+}
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+import { IUser } from './../interfaces/iuser.interfaces';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { lastValueFrom, Observable } from 'rxjs';
+import { IResponse } from '../interfaces/iresponse.nterfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private baseUrl: string = 'https://peticiones.online/api/users';
+  private httpClient = inject(HttpClient);
 
-  constructor() { }
+  //obtener todos los usuarios
+  getAll(): Observable<IResponse> {
+    return this.httpClient.get<IResponse>(this.baseUrl);
+  }
+
+  getAllPromise(): Promise<IResponse> {
+    return lastValueFrom(this.httpClient.get<IResponse>(this.baseUrl));
+  }
+
+  //Obtener un usuario por id
+  getUserById(_id: string): Observable<IUser> {
+    return this.httpClient.get<IUser>(`${this.baseUrl}/${_id}`);
+  }
+
+  // Método para crear un nuevo usuario
+  createUser(user: IUser): Observable<IUser> {
+    return this.httpClient.post<IUser>(this.baseUrl, user);
+  }
+
+  //actualizar
+  updateUser(id: number, user: any): Observable<any> {
+    return this.httpClient.put<any>(`${this.baseUrl}/${id}`, user);
+  }
+
+ //Eliminar un usuario
+ deleteUser(id: number): Observable<IUser>{
+  return this.httpClient.delete<any>(`${this.baseUrl}/${id}`);
+
+ }
 }
